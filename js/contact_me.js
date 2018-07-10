6ca6244ea58b43dd8090607e2529111581647954
Ruslan Gainanov <rgaynanov@nas.company>
2018-07-10 11:45:53 +0500

    Fix https. Add correct s3t.club url

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "http://formspree.io/gromrx1@gmail.com",
+                url: "https://formspree.io/gromrx1@gmail.com",
                 method: "POST",
                 data: {
                     name: name,
