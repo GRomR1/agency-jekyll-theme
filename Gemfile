d9e61287dd79b851ef0e1ab14d63d77e8f9fa54d
Ruslan Gainanov <rgaynanov@nas.company>
2018-07-10 11:20:08 +0500

    Init s3t

---
+++ Gemfile
@@ -0,0 +1,8 @@
+#     bundle install
+#     bundle exec jekyll serve
+
+source "https://rubygems.org"
+
+gem "jekyll", "~> 3.8.0"
+
+gem "agency-jekyll-theme"
--- js/contact_me.js
+++ js/contact_me.js
@@ -18,22 +18,23 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
+                url: "//formspree.io/gromrx1@gmail.com",
+                method: "POST",
                 data: {
                     name: name,
                     phone: phone,
                     email: email,
-                    message: message
+                    message: message,
                 },
+                dataType: "json",
                 cache: false,
-                success: function() {
+                success: function(data) {
                     // Success message
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Ваше сообщение отправлено.</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
